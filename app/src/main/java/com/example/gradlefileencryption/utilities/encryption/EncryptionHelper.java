48a39516b96b5dd14a7fc6f4f3e02ec6ce64d0a1
Sahil Dudeja <sahildudeja@icloud.com>
2021-03-11 21:11:35 +0530

    Add dummy file and todo

--- app/src/main/java/com/example/gradlefileencryption/utilities/encryption/EncryptionHelper.java
+++ app/src/main/java/com/example/gradlefileencryption/utilities/encryption/EncryptionHelper.java
@@ -14,11 +14,13 @@ public class EncryptionHelper {
     private static final byte[] IV = new byte[SIZE_BYTE];
 
     public static String getEncryptedString(String text) {
+        // todo: this is not a secure way to get KEY and SALT via BuildConfig file
         Encryption encryption = Encryption.getDefault(BuildConfig.ENC_KEY, BuildConfig.ENC_SALT, IV);
         return encryption == null ? "" : encryption.encryptOrNull(text);
     }
 
     public static String getDecryptedString(String text) {
+        // todo: Got values from BuildConfig for sample usage only
         Encryption encryption = Encryption.getDefault(BuildConfig.ENC_KEY, BuildConfig.ENC_SALT, IV);
         return encryption == null ? "" : encryption.decryptOrNull(text);
     }
